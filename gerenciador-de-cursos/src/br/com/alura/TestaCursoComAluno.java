8a12d6f9380be56fdbf5938019895725c1348528
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-11 15:17:15 -0300

    Aplicando o Set no modelo

---
+++ gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -0,0 +1,27 @@
+package br.com.alura;
+
+public class TestaCursoComAluno {
+
+	public static void main(String[] args) {
+		Curso javaColecoes = new Curso("Dominando as coleções do Java", 
+				"Paulo Silveira");
+		//javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
+		
+		Aluno a1 = new Aluno("Lucas Andrade", 34672);
+		Aluno a2 = new Aluno("Walter Souza", 5617);
+		Aluno a3 = new Aluno("Murilo Rocha", 17645);
+		
+		javaColecoes.matricula(a1);
+		javaColecoes.matricula(a2);
+		javaColecoes.matricula(a3);
+		
+		System.out.println("Todos os alunos matriculados: ");
+		javaColecoes.getAlunos().forEach(a -> {
+			System.out.println(a);
+		});
+	}
+
+}
