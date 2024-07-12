62d6fcaf0b28187c1eaac8f04cb8f7c76cba3699
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-12 10:19:18 -0300

    TreeSet e Comparator

--- gerenciador-de-cursos/src/br/com/alura/OrdenaPorIdade.java
+++ gerenciador-de-cursos/src/br/com/alura/OrdenaPorIdade.java
@@ -1,12 +1,13 @@
 package br.com.alura;
 
 import java.util.Comparator;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
 
-public abstract class OrdenaPorIdade implements Comparator<Funcionario>{
+public class OrdenaPorIdade implements Comparator<Funcionario>{
 
+	@Override
+	public int compare(Funcionario funcionario, Funcionario outroFuncionario) {
+	        return funcionario.getIdade() - outroFuncionario.getIdade();
+	    }
 }
 
 
